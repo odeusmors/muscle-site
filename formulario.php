c7ef9c293aefe05a6fa4a9d0a69b9ae4010c7bc2
whoami <101435592+odeusmors@users.noreply.github.com>
2025-01-18 01:22:48 -0300

    update

--- formulario.php
+++ formulario.php
@@ -1,10 +1,49 @@
+<?php
+
+    if(isset($_POST['submit']))
+    {
+        // print_r('Nome: ' . $_POST['nome']);
+        // print_r('<br>');
+        // print_r('Email: ' . $_POST['email']);
+        // print_r('<br>');
+        // print_r('Telefone: ' . $_POST['telefone']);
+        // print_r('<br>');
+        // print_r('Sexo: ' . $_POST['genero']);
+        // print_r('<br>');
+        // print_r('Data de nascimento: ' . $_POST['data_nascimento']);
+        // print_r('<br>');
+        // print_r('Cidade: ' . $_POST['cidade']);
+        // print_r('<br>');
+        // print_r('Estado: ' . $_POST['estado']);
+        // print_r('<br>');
+        // print_r('Endereço: ' . $_POST['endereco']);
+
+        include_once('config.php');
+
+        $nome = $_POST['nome'];
+        $email = $_POST['email'];
+        $senha = $_POST['senha'];
+        $telefone = $_POST['telefone'];
+        $sexo = $_POST['genero'];
+        $data_nasc = $_POST['data_nascimento'];
+        $cidade = $_POST['cidade'];
+        $estado = $_POST['estado'];
+        $endereco = $_POST['endereco'];
+
+        $result = mysqli_query($conexao, "INSERT INTO usuarios(nome,senha,email,telefone,sexo,data_nasc,cidade,estado,endereco) 
+        VALUES ('$nome','$senha','$email','$telefone','$sexo','$data_nasc','$cidade','$estado','$endereco')");
+
+        header('Location: login.php');
+    }
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulário | GN</title>
+    <title>Formulário | Muscle</title>
     <style>
         body{
             font-family: Arial, Helvetica, sans-serif;
@@ -80,14 +119,20 @@
     </style>
 </head>
 <body>
+    <a href="home.php">Voltar</a>
     <div class="box">
-        <form action="">
+        <form action="formulario.php" method="POST">
             <fieldset>
                 <legend><b>Fórmulário de Clientes</b></legend>
                 <br>
                 <div class="inputBox">
                     <input type="text" name="nome" id="nome" class="inputUser" required>
                     <label for="nome" class="labelInput">Nome completo</label>
+                </div>
+                <br>
+                <div class="inputBox">
+                    <input type="password" name="senha" id="senha" class="inputUser" required>
+                    <label for="senha" class="labelInput">Senha</label>
                 </div>
                 <br><br>
                 <div class="inputBox">
